a41a14f405500b3b1a67db6bb3b3904b9b7c1e7c
Prabhav Deshpande <prabhavdeshpande89@gmail.com>
2026-09-09 12:53:25 +0530

    Connect frontend to EC2 backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 import jsPDF from "jspdf";
 import "./App.css";
 
-const API_URL = "http://15.252.182.106:8000";
+const API_URL = "/api";
 
 function App() {
   // ============================================================
